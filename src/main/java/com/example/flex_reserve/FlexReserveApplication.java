81a96bf0d785cb94c7b885c6ddacb52dc12aaafd
shinv <shin-production@mail.ru>
2025-11-26 23:13:51 +0100

    Spring initilizr first commit

---
+++ src/main/java/com/example/flex_reserve/FlexReserveApplication.java
@@ -0,0 +1,13 @@
+package com.example.flex_reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flex_reserve/FlexReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flex_reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
