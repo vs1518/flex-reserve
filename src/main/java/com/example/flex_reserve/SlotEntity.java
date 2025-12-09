82880671599b2b3d996c02624e83b2e5354ed542
shinv <shin-production@mail.ru>
2025-12-09 11:32:54 +0100

    Initialisation of backend and BDD

--- src/main/java/com/example/flex_reserve/SlotEntity.java
+++ src/main/java/com/example/flex_reserve/SlotEntity.java
@@ -3,26 +3,12 @@ package com.example.flex_reserve;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-@Entity
-public class SlotEntity {
+@Entity public class SlotEntity {
 
     @Id
-    private Long id;
 
+    private Long id;
     private String name;
     private String surname;
 
-    public SlotEntity() {
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getSurname() { return surname; }
-    public void setSurname(String surname) { this.surname = surname; }
 }
-
-
